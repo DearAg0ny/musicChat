73e265644ffeac4a089d76dc5b52870a5609d393
DearAg0ny <deimas3133@gmail.com>
2023-06-28 23:14:03 +0300

    Auth fix

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class LoginController extends Controller
 {
@@ -35,5 +37,18 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+    public function login(Request $request) {
+        $incomingFields = $request->validate([
+            'name'=>'required',
+            'password'=>'required'
+        ]);
+        if (auth()->attempt([
+            'name'=>$incomingFields['name'],
+            'password'=>$incomingFields['password']
+        ])) {
+            $request->session()->regenerate();
+        }
+        return redirect('/');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -10,11 +10,16 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SongsController;
+use App\Http\Controllers\Auth\LoginController;
 
 
 Auth::routes();
+//Auth Controller
+//Route::post('/login', [LoginController::class, 'login']);
 
 Route::get('/', 'HomeController@index')->name('home');
 Route::resource('channels', 'ChannelsController');
